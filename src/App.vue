d12cb28f14a9b4df00cc0f839891d26d66039ae7
Stefan Peters <Stefan.Peters@gbv.de>
2022-11-14 12:59:02 +0100

    Add coli-conc page style (including fonts and necessary changes)

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,55 @@
 <template>
-  <header>
+  <header class="header">
+    <a
+      href="https://coli-conc.gbv.de/"
+      class="coli-conc-logo-small">
+      <img
+        src="https://coli-conc.gbv.de/images/coli-conc.svg"
+        alt="coli-conc Logo">
+    </a>
     <h1>
       JSKOS Server Web
     </h1>
-    <div style="flex: 1" />
-    <user-status>
-      <template #before>
-        <p>
-          Select JSKOS Server:
-        </p>
-        <ul>
-          <li
-            v-for="server in state.config.jskos"
-            :key="server.url"
-            :class="{
-              'selected-server': server === state.selectedServer,
-            }">
-            <a
-              href=""
-              @click.prevent="state.setSelectedServer(server)">
-              {{ (server.about && server.about.config.title) || server.url }}
-            </a>
-          </li>
-        </ul>
-        <hr>
-      </template>
-    </user-status>
+    <ul class="menu">
+      <li>
+        <a
+          href="https://coli-conc.gbv.de/coli-ana/"
+          title="Go to coli-conc website">
+          ⬅ back to coli-conc website
+        </a>
+      </li>
+      <li>
+        <user-status>
+          <template #before>
+            <p>
+              Select JSKOS Server:
+            </p>
+            <ul>
+              <li
+                v-for="server in state.config.jskos"
+                :key="server.url"
+                :class="{
+                  'selected-server': server === state.selectedServer,
+                }">
+                <a
+                  href=""
+                  @click.prevent="state.setSelectedServer(server)">
+                  {{ (server.about && server.about.config.title) || server.url }}
+                </a>
+              </li>
+            </ul>
+            <hr>
+          </template>
+        </user-status>
+      </li>
+    </ul>
+    <div style="clear:both" />
   </header>
-  <h1>
-    Test
-  </h1>
+  <main id="main">
+    <div class="section">
+      <h2>...</h2>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -57,21 +77,22 @@ export default defineComponent({
 </script>
 
 <style>
-header {
-  display: flex;
-  align-items: center;
+.user-status {
+  z-index: 9999;
+}
+.user-status > a {
+  font-size: initial;
+}
+header .user-status li {
+  float: none;
+}
+header .user-status li a {
+  text-align: left;
 }
 header > h1 {
-  font-size: 20px;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 60px auto 0 auto;
-  padding: 0 20px;
-  max-width: 800px;
+  float: left;
+  padding: 18px 0 0 20px;
+  font-size: 24px;
 }
 li.selected-server {
   background-color: #feffcc;
