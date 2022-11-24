324e1f056e78f35528526d822910aa9546294f16
Stefan Peters <Stefan.Peters@gbv.de>
2022-11-24 13:01:29 +0100

    Home: Add additional example mappings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,12 @@
     <p>
       <router-link to="/?uri=https://coli-conc.gbv.de/api/mappings/761fe565-3815-4c14-b277-4fc980639ced">
         Example Mapping
+      </router-link> ·
+      <router-link to="/?uri=https://coli-conc.gbv.de/api/mappings/15d5cc0a-1a31-42db-9b90-861363de16fb">
+        Example Mapping (1-to-n)
+      </router-link> ·
+      <router-link to="/?uri=https://coli-conc.gbv.de/api/mappings/92769cf4-1f76-4bd1-ad2d-8c92f4ede20e">
+        Example Mapping (1-to-0)
       </router-link>
     </p>
   </div>
