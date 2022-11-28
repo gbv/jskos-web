44fcf13c7dfde21ec5585540dc1f950238f57c61
Stefan Peters <Stefan.Peters@gbv.de>
2022-11-28 12:44:56 +0100

    Determine default locale through the user's browser

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,18 @@
 import { createI18n } from "vue-i18n"
 import messages from "@/../locale.json"
+
+// Determine default locale through the user's browser
+let locale
+for (const lang of navigator.languages || []) {
+  if (lang.startsWith("de") || lang.startsWith("en")) {
+    locale = lang.slice(0, 2)
+    break
+  }
+}
+
 export default createI18n({
   legacy: false,
-  locale: "de",
+  locale: locale || "en",
   fallbackLocale: "en",
   messages,
 })
